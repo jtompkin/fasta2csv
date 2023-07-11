2097c21d924493df740c4d23ed7a291b51e52783
jtompkin <tompkinjo@gmail.com>
2023-07-11 17:27:48 -0400

    Setup files

---
+++ src/fasta2csv/fasta2csv.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+"""
+=============
++ fasta2csv +
+=============
+
+author: Josh Tompkin
+contact: tompkinjo@gmail.com
+home: https://github.com/jtompkin/fasta2csv
+"""
+import argparse
+import sys
+import os
+
+def csv2fasta():
+    pass
+
+def fasta2csv():
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
